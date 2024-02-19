ad73af0a90002ed2e6d18d3c911c054585c2990b
demianov-an <alexm92@inbox.ru>
2024-02-19 19:41:14 +0600

    1.1 - Get-запрос - Задача

--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,35 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+export const decksAPI = {
+  getDecks() {
+    return instance.get<FetchDecksResponseType>('v2/decks')
+  },
+}
+
+export type FetchDecksResponseType = {
+  'items': [
+    {
+      'author': {
+        'id': string
+        'name': string
+      },
+      'id': string
+      'userId': string
+      'name': string
+      'isPrivate': boolean
+      'cover': string
+      'created': string
+      'updated': string
+      'cardsCount': number
+    }
+  ],
+  'pagination': {
+    'currentPage': number
+    'itemsPerPage': number
+    'totalPages': number
+    'totalItems': number
+  },
+  'maxCardsCount': number
+}
